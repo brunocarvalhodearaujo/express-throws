6ac00bd4f2dbb9d0107fdc523882c8d4f0641fee
Bruno Carvalho de Araujo <brunocarvalhodearaujo@gmail.com>
2018-02-18 11:37:15 -0300

    fix babel

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@ const HttpError = require('./HttpError')
 
 Object.defineProperty(exports, '__esModule', { value: true })
 
-exports.default = middleware
-module.exports = { HttpError }
+Object.defineProperty(exports, 'HttpError', {
+  enumerable: true,
+  get: () => interopRequireDefault(HttpError).default
+})
+
+Object.defineProperty(exports, 'default', {
+  enumerable: true,
+  get: () => interopRequireDefault(middleware).default
+})
+
+function interopRequireDefault (obj) {
+  return obj && obj.__esModule
+    ? obj
+    : { default: obj }
+}
