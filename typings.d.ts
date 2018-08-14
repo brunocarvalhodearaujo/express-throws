e42939ca5018fdc9b2e15b034f2025c2176b8b9c
Bruno Carvalho de Araujo <brunocarvalhodearaujo@gmail.com>
2018-08-14 08:54:51 -0300

    fix typings

--- typings.d.ts
+++ typings.d.ts
@@ -1,5 +1,7 @@
+import { Router } from 'express'
+
 export declare class HttpError extends Error {
   constructor(code: number, message: string, type?: string)
 }
 
-export default function (): void
+export default function (): Router
